b29c4ddfb4be48e1eef304c64254d645bf05a2db
Dominik <Dominique.Wellisch@t-online.de>
2025-01-18 18:31:38 +0100

    Projektdateien hinzufügen.

---
+++ Handlers/serverHandler.cs
@@ -0,0 +1,20 @@
+﻿using MapEditorReborn.API.Features.Objects;
+using SCP294.Classes;
+using SCP294.Types;
+using System.Collections.Generic;
+using VoiceChat.Codec;
+
+namespace SCP294.handlers
+{
+    public class serverHandler
+    {
+        public void WaitingForPlayers() {
+            SCP294.Instance.SpawnedSCP294s = new Dictionary<SchematicObject, bool>();
+            SCP294.Instance.PlayersNear294 = new List<string>();
+            SCP294.Instance.CustomDrinkItems = new Dictionary<ushort, DrinkInfo>();
+            SCP294.Instance.PlayerVoicePitch = new Dictionary<string, float>();
+            SCP294.Instance.Encoders = new Dictionary<ReferenceHub, OpusComponent>();
+            SCP294Object.SpawnSCP294();
+        }
+    }
+}
